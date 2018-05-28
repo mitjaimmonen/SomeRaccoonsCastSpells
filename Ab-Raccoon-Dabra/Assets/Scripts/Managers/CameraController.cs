24551c7cd59613af1e491235b495d44321ec0f84
Unknown <mitja.immonen@hotmail.com>
2018-05-28 19:52:33 +0300

    Inputs, basic player controlling

---
+++ Ab-Raccoon-Dabra/Assets/Scripts/Managers/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	    public float angle;
+        public float distance;
+        public Transform target;
+		public float lerpSpeed = 10f;
+
+		private Vector3 camPos, camDir;
+
+        void LateUpdate()
+        {
+            float angle = Mathf.Deg2Rad * (this.angle);
+            float horizontal = Mathf.Cos(angle) * distance;
+            float vertical = Mathf.Sin(angle) * distance;
+
+			camDir = -Vector3.forward;
+			Vector3 direction = new Vector3(camDir.x, 0, camDir.z);
+			direction.Normalize();
+			direction = direction * horizontal;
+
+            camPos = target.position;			
+			camPos += new Vector3(direction.x, vertical, direction.z);
+
+            transform.position = Vector3.Lerp(transform.position, camPos, Time.deltaTime * lerpSpeed);
+            transform.LookAt(target);
+			var euler = transform.localEulerAngles;
+			euler.y = 0;
+			transform.localEulerAngles = euler;
+	}
+}
---
+++ Ab-Raccoon-Dabra/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure; // Required in C#
+
+public class InputManager : MonoBehaviour {
+		public PlayerController playerController;
+        bool playerIndexSet = false;
+        PlayerIndex playerIndex;
+        GamePadState state;
+        GamePadState prevState;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Update()
+    {
+        // Find a PlayerIndex, for a single player game
+        // Will find the first controller that is connected ans use it
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+        playerController.HandleInput(state, prevState);
+    }
+
+}
